0e012a18707aa99ba254aed528a33a4e0ceae64a
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2023-02-02 15:59:04 -0300

    Criação da classe Pessoa, com membros e construtor

---
+++ Cursos_Fundamentos_OOP/ConsoleAppExemplo1/Classes/Pessoa.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Aula_Classes1.Classes
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public char Sexo { get; set; }
+
+        public Pessoa(string nome, int idade, char sexo)
+        {
+            Nome = nome;
+            Idade = idade;
+            Sexo = sexo;
+        }
+
+        public void Identificar()
+        {
+            Console.WriteLine($" Olá, meu nome é {Nome}, tenho {Idade} ano(s). Sou do Sexo {Sexo}");
+        }
+    }
+}
--- Cursos_Fundamentos_OOP/ConsoleAppExemplo1/Program.cs
+++ Cursos_Fundamentos_OOP/ConsoleAppExemplo1/Program.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using Aula_Classes1.Classes;
+using System;
 
-namespace ConsoleAppExemplo1
+namespace Aula_Classes1
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Pessoa pessoa1 = new Pessoa("Alexandre",48,'M');
+            Pessoa pessoa2 = new Pessoa("Luciana", 46, 'F');
+
+            
+
+            pessoa1.Identificar();
+            pessoa2.Identificar();
         }
     }
 }
