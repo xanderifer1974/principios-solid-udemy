be90ea85e2649e4169cfcfd331d6de8059b5959d
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2023-02-04 22:34:08 -0300

    Criando um método de extensão

---
+++ Cursos_Fundamentos_OOP/MethodosExtensao/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using MethodosExtensao.Utils;
+
+
+namespace MethodosExtensao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var texto1 = "curso";
+            var texto2 = "exemplo";
+
+            //Método de extensão
+
+            texto1 = texto1.CaixaAltaDaPrimeiraLetra();
+            texto2 = texto2.CaixaAltaDaPrimeiraLetra();
+
+            Console.WriteLine(texto1);
+            Console.WriteLine(texto2);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Cursos_Fundamentos_OOP/MethodosExtensao/Utils/MetodosExtensao.cs
@@ -0,0 +1,17 @@
+﻿namespace MethodosExtensao.Utils
+{
+    public static  class MetodosExtensao
+    {
+        public static string CaixaAltaDaPrimeiraLetra(this string valor)
+        {
+            if (valor.Length > 0)
+            {
+                char[] array = valor.ToCharArray();
+                array[0] = char.ToUpper(array[0]);
+                return new string(array);
+            }
+
+            return valor;
+        }
+    }
+}
